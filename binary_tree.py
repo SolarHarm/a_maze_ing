e56a011b86fdec048edacb4a4dc0970c06a50b15
Harm Aalbers <haalbers@gmail.com>
2020-10-08 00:24:01 +0200

    Add Binary Tree method for craving a maze

---
+++ binary_tree.py
@@ -0,0 +1,23 @@
+from grid import Grid, NORTH, EAST
+from random import choice as randChoice
+
+
+class BinaryTree:
+    """
+    Create a new maze with the help of the binary tree method.
+    """
+
+    def __init__(self, grid: Grid):
+        self.grid = grid
+
+    def create(self) -> Grid:
+        for cell in self.grid.get_per_cell():
+            choices = [
+                c
+                for c in [cell.get_neighbour(NORTH), cell.get_neighbour(EAST)]
+                if c != None
+            ]
+            if len(choices) != 0:
+                cell.link(randChoice(choices))
+
+        return self.grid
